96c149d68349155b3da10a05b1426c6ab0ff501e
afrihar <afrihar@gmail.com>
2024-06-28 21:28:33 +0700

    init project

---
+++ src/main/java/com/example/springbootkafkarabbitmqpostgres/SpringbootKafkaRabbitmqPostgresApplication.java
@@ -0,0 +1,13 @@
+package com.example.springbootkafkarabbitmqpostgres;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootKafkaRabbitmqPostgresApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(SpringbootKafkaRabbitmqPostgresApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/example/springbootkafkarabbitmqpostgres/SpringbootKafkaRabbitmqPostgresApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springbootkafkarabbitmqpostgres;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootKafkaRabbitmqPostgresApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
