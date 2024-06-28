eaba2069c1c4e910d9687096a2535e0230a28108
afrihar <afrihar@gmail.com>
2024-06-29 01:12:03 +0700

    define Table name ke "users"

--- src/main/java/com/example/springbootkafkarabbitmqpostgres/entity/User.java
+++ src/main/java/com/example/springbootkafkarabbitmqpostgres/entity/User.java
@@ -1,10 +1,7 @@
 package com.example.springbootkafkarabbitmqpostgres.entity;
 
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
@@ -13,6 +10,7 @@ import lombok.ToString;
 @Getter
 @Setter
 @ToString
+@Table(name = "users")
 public class User {
   @Id
   @GeneratedValue(strategy = GenerationType.IDENTITY)
