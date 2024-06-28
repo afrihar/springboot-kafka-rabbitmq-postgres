cae31bf4ade37388885233bf439c23e69dfa38a0
afrihar <afrihar@gmail.com>
2024-06-29 06:20:09 +0700

    modify RabbitMQ Config

--- src/main/java/com/example/springbootkafkarabbitmqpostgres/config/RabbitMQConfig.java
+++ src/main/java/com/example/springbootkafkarabbitmqpostgres/config/RabbitMQConfig.java
@@ -1,11 +1,24 @@
 package com.example.springbootkafkarabbitmqpostgres.config;
 
 import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.rabbit.connection.CachingConnectionFactory;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class RabbitMQConfig {
+
+  @Bean
+  public ConnectionFactory connectionFactory() {
+    return new CachingConnectionFactory("rabbitmq", 5672);
+  }
+
+  @Bean
+  public RabbitTemplate rabbitTemplate() {
+    return new RabbitTemplate(connectionFactory());
+  }
 
   @Bean
   public Queue userQueue() {
