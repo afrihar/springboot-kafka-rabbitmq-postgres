2c345e83352283fa21f8e026df181ab279c4f503
afrihar <afrihar@gmail.com>
2024-06-29 08:48:02 +0700

    add Scheduler to send keep-alive every 15 seconds

--- src/main/java/com/example/springbootkafkarabbitmqpostgres/SpringbootKafkaRabbitmqPostgresApplication.java
+++ src/main/java/com/example/springbootkafkarabbitmqpostgres/SpringbootKafkaRabbitmqPostgresApplication.java
@@ -2,8 +2,10 @@ package com.example.springbootkafkarabbitmqpostgres;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class SpringbootKafkaRabbitmqPostgresApplication {
 
   public static void main(String[] args) {
